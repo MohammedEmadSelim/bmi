f8f32b3b4cee2a63083d60c8e7919ddd0807c88d
3bdoEssam22 <abdulrahman.e.f22@gmail.com>
2026-01-24 15:17:45 +0200

    First_Session_Flutter

---
+++ lib/app_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+static const Color primary = Color(0xff7876CD);
+static const Color darkPurple = Color(0xff484783);
+static const Color white = Color(0xffFFFFFF);
+static const Color greyWhite = Color(0x80F8F9FF);
+
+
+}
---
+++ lib/on_boarding.dart
@@ -0,0 +1,70 @@
+import 'package:bmi/app_color.dart';
+import 'package:flutter/material.dart';
+
+class OnBoarding extends StatelessWidget {
+  const OnBoarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset("Assets/Images/first_img.png"),
+            Container(
+              decoration: BoxDecoration(
+                color: AppColor.primary,
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(30),
+                child: Column(
+                  children: [
+                    Text(
+                      "Know Your Body Better , Get Your BMI Score in Less Than a Minute!",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                        color: AppColor.white,
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                    Text(
+                      "It takes just 30 seconds – and your health is worth it!",
+                      style: TextStyle(color: AppColor.white, fontSize: 16),
+                    ),
+                    SizedBox(height: 40),
+                    Divider(thickness: 3),
+                    SizedBox(height: 20,),
+                    SizedBox(
+                      width: 300,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: AppColor.darkPurple,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadiusGeometry.circular(8),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          "Get Start",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                            color: AppColor.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
