24bd4a5eb5f52a7492119204773213023ccee1a4
Mohammed <mohammedemad557@yahoo.com>
2026-01-24 14:08:48 +0200

    onboarding screen

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors{
+static const  Color primary = Color(0xff7876CD);
+static const  Color darkPurple = Color(0xff484783);
+static const  Color white = Color(0xffFFFFFF);
+static const Color greyWhite = Color(0x80F8F9FF); // 50% opacity greyish-white color
+
+}
