c593ed193b89d8b118ef8437453195a48805a9de
caesar <abdulhamidiskander@gmail.com>
2026-01-25 10:38:09 +0200

    add onboarding screen

---
+++ lib/onboarding.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+class Onboarding extends StatelessWidget {
+  const Onboarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment .center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+          Image.asset(
+          "assets/img.png",
+          height: 300,
+          fit: BoxFit.contain,
+        ) ,
+            SizedBox(height: 5,)
+            , Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),              color: Colors.deepPurpleAccent
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Text("Know Your Body Better , Get Your BMI Score in Less Than a Minute!" , style: TextStyle(
+                      fontSize: 28 , fontWeight: FontWeight.bold
+                        ,color: Colors.white
+                    ),)
+                    , SizedBox(height: 10,),
+                    Text("It takes just 30 seconds – and your health is worth it!" , style: TextStyle(color: Colors.white),),
+                     SizedBox(height: 20,),
+                    Divider(),
+                    ElevatedButton(onPressed: (){},
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.deepPurple
+                        ,foregroundColor: Colors.white
+                        )
+                        ,child:Text("Get Started"))
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
