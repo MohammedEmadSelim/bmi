2096ec96b502704aaac456fca9d0aff1d7af418f
Mohammed <mohammedemad557@yahoo.com>
2026-01-30 14:31:38 +0200

    second part of result screen

--- lib/core/theme/colors.dart
+++ lib/core/theme/colors.dart
@@ -4,6 +4,7 @@ class AppColors{
 static const  Color primary = Color(0xff7876CD);
 static const  Color darkPurple = Color(0xff484783);
 static const  Color white = Color(0xffFFFFFF);
+static const  Color green = Color(0xff01502E);
 static const Color greyWhite = Color(0x80F8F9FF); // 50% opacity greyish-white color
 
 }
--- lib/view/result_screen.dart
+++ lib/view/result_screen.dart
@@ -7,173 +7,217 @@ class ResultScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12.0),
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: AppColors.primary,
-                borderRadius: BorderRadius.circular(16)
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      Row(
-                        children: [
-                          Column(
-                            children: [
-                              Text(
-                                "Samy Call",
-                                style: TextStyle(
-                                  color: AppColors.white,
 
-                                  fontSize: 22,
-
-                                  fontWeight: FontWeight.w700,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: AppColors.primary,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            Column(
+                              children: [
+                                Text(
+                                  "Samy Call",
+                                  style: TextStyle(
+                                    color: AppColors.white,
+        
+                                    fontSize: 22,
+        
+                                    fontWeight: FontWeight.w700,
+                                  ),
                                 ),
-                              ),
-                              SizedBox(height: 10,),
-                              Text(
-                                "A 23 years old male.",
-                                style: TextStyle(
-                                  color: AppColors.white,
-                                  fontSize: 15,
-
-                                  fontWeight: FontWeight.w400,
+                                SizedBox(height: 10),
+                                Text(
+                                  "A 23 years old male.",
+                                  style: TextStyle(
+                                    color: AppColors.white,
+                                    fontSize: 15,
+        
+                                    fontWeight: FontWeight.w400,
+                                  ),
                                 ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 10,),
-                      Row(
-                        children: [
-                          Column(
-                            children: [
-                              Text(
-                                "36.2",
-                                style: TextStyle(
-                                  color: AppColors.white,
-
-                                  fontSize: 35,
-
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              ),
-                              SizedBox(height: 6,),
-                              Text(
-                                "A 23 years old male.",
-                                style: TextStyle(
-                                  color: AppColors.white,
-                                  fontSize: 18,
-
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 10,),
-                      Row(
-                        children: [
-
-                          Column(
-                            children: [
-                              Text(
-                                "180 CM",
-                                style: TextStyle(
-                                  color: AppColors.white,
-
-                                  fontSize: 20,
-
-                                  fontWeight: FontWeight.w700,
-                                ),                              ),
-                              SizedBox(height: 6,),
-                              Text(
-                                "Height",
-                                style: TextStyle(
-                                  color: AppColors.white,
-                                  fontSize: 18,
-
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(width: 12,),
-                          SizedBox(
-                            height: 55,
-                            child: VerticalDivider(
-                              color: AppColors.white,
+                              ],
                             ),
-                          ),
-                          SizedBox(width: 12,),
-                          Column(
-                            children: [
-                              Text(
-                                "70 Kg",
-                                style: TextStyle(
-                                  color: AppColors.white,
-
-                                  fontSize: 20,
-
-                                  fontWeight: FontWeight.w700,
-                                ),                              ),
-                              SizedBox(height: 6,),
-                              Text(
-                                "weight",
-                                style: TextStyle(
-                                  color: AppColors.white,
-                                  fontSize: 18,
-
-                                  fontWeight: FontWeight.w500,
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Column(
+                              children: [
+                                Text(
+                                  "36.2",
+                                  style: TextStyle(
+                                    color: AppColors.white,
+        
+                                    fontSize: 35,
+        
+                                    fontWeight: FontWeight.w700,
+                                  ),
                                 ),
-                              ),
-                            ],
-                          ),
-                        ],
+                                SizedBox(height: 6),
+                                Text(
+                                  "A 23 years old male.",
+                                  style: TextStyle(
+                                    color: AppColors.white,
+                                    fontSize: 18,
+        
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Column(
+                              children: [
+                                Text(
+                                  "180 CM",
+                                  style: TextStyle(
+                                    color: AppColors.white,
+        
+                                    fontSize: 20,
+        
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                                SizedBox(height: 6),
+                                Text(
+                                  "Height",
+                                  style: TextStyle(
+                                    color: AppColors.white,
+                                    fontSize: 18,
+        
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(width: 12),
+                            SizedBox(
+                              height: 55,
+                              child: VerticalDivider(color: AppColors.white),
+                            ),
+                            SizedBox(width: 12),
+                            Column(
+                              children: [
+                                Text(
+                                  "70 Kg",
+                                  style: TextStyle(
+                                    color: AppColors.white,
+        
+                                    fontSize: 20,
+        
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                                SizedBox(height: 6),
+                                Text(
+                                  "weight",
+                                  style: TextStyle(
+                                    color: AppColors.white,
+                                    fontSize: 18,
+        
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Image.asset('assets/images/Vector.png'),
+                  ],
+                ),
+              ),
+              SizedBox(height: 24),
+        
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 35,horizontal: 16),
+                decoration: BoxDecoration(
+                  color: AppColors.green,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Under Weight",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 22,
+                        color: AppColors.white,
                       ),
-                    ],
-                  ),
-                  Image.asset('assets/images/Vector.png'),
-                ],
-              ),
-            ),
-            Container(),
-            SizedBox(
-              width: 300,
-              height: 45,
-              child: ElevatedButton(
-                onPressed: () {
-                  /// navigate to mew screen
-
-                  Navigator.of(
-                    context,
-                  ).push(MaterialPageRoute(builder: (context) => ResultScreen()));
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(
-                      16,
-                    ), // Adjust the radius here
-                  ),
-
-                  backgroundColor: AppColors.darkPurple,
-                ),
-                child: Text(
-                  "GCalculate BMI Again",
-                  style: TextStyle(color: Colors.white),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      "Under Weight",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 15,
+                        color: AppColors.white,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+        
+                    Text(
+                      "Lorem ipsum dolor sit amet consectetur. Sagittis interdum dui enim imperdiet sapien cursus velit pharetra. Viverra justo tempor dictum odio. Nisl non dui integer orci nulla eget laoreet tellus. Orci nunc a orci convallis ac orci. Urna auctor at elementum sit ante maecenas ullamcorper rhoncus dictum. Morbi venenatis lectus ultrices euismod. Laoreet purus risus amet enim sagittis ut. Consectetur libero orci urnager dignissi est.",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 15,
+                        color: AppColors.white,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ],
+              SizedBox(height: 24,),
+        
+              SizedBox(
+                width: 300,
+                height: 45,
+                child: ElevatedButton(
+                  onPressed: () {
+                    /// navigate to mew screen
+        
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => ResultScreen()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                        16,
+                      ), // Adjust the radius here
+                    ),
+        
+                    backgroundColor: AppColors.darkPurple,
+                  ),
+                  child: Text(
+                    "GCalculate BMI Again",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
