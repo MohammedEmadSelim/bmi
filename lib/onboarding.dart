9bfa52ee299e8601da8d13ca69009c531ac21303
caesar <abdulhamidiskander@gmail.com>
2026-01-30 13:52:06 +0200

    Refactor onboarding screen into custom widgets

--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:bmi/final_screen.dart';
+import 'package:bmi/widgets/onboarding_image.dart';
+import 'package:bmi/widgets/onboarding_card.dart';
+
 class Onboarding extends StatelessWidget {
   const Onboarding({super.key});
 
@@ -12,45 +14,9 @@ class Onboarding extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment .center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-          Image.asset(
-          "assets/img.png",
-          height: 300,
-          fit: BoxFit.contain,
-        ) ,
+          OnboardingImage(),
             SizedBox(height: 5,)
-            , Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
-                  ),              color: Colors.deepPurpleAccent
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Text("Know Your Body Better , Get Your BMI Score in Less Than a Minute!" , style: TextStyle(
-                      fontSize: 28 , fontWeight: FontWeight.bold
-                        ,color: Colors.white
-                    ),)
-                    , SizedBox(height: 10,),
-                    Text("It takes just 30 seconds – and your health is worth it!" , style: TextStyle(color: Colors.white),),
-                     SizedBox(height: 20,),
-                    Divider(),
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(onPressed: (){
-                        Navigator.push(context, MaterialPageRoute(builder: (context)=>FinalScreen()));
-                      },
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.white
-                          ,foregroundColor: Colors.deepPurple
-                          )
-                          ,child:Text("Get Started")),
-                    )
-                  ],
-                ),
-              ),
-            )
+            , OnboardingCard()
           ],
         ),
       ),
